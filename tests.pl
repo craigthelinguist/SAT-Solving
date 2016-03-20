c470aed3d378f166af55ae9c47428d3efead25f5
craigthelinguist <aaroncraig@protonmail.ch>
2016-03-21 00:40:22 +1300

    Add testing framework, fix bug in stree
    the eval predicate did not have a case for negation

--- tests.pl
+++ tests.pl
@@ -1,33 +1,111 @@
 
-:- [stree].
+:- [sat].
 
 
-:-
-   ((stree([+, x, y], _), !,
-   write('Test 1 passed.')) ; write('Test 1 failed.')), nl. 
+%% Predicates for formatting output.
+%% ======================================================================
 
-:-
-   ((stree([+, x, [~, x]], _), !,
-   write('Test 2 passed.')) ; write('Test 2 failed.')), nl.
-    
-:-
-   ((\+ stree([*, x, [~, x]], _),
-   write('Test 3 passed.')) ; write('Test 3 failed.')), nl.
+join([], []).
+
+join([X|Xs], Result) :-
+   join(Xs, RHS),
+   concat(X, RHS, Result).   
+
+to_ccodes(X, S) :-
+   atom(X),
+   atom_codes(X, S), !.
+
+to_ccodes([~, X], R) :-
+   to_ccodes(X, S1),
+   join(["[", "~", ",", " ", S1, "]"], R), !.
+
+to_ccodes([+, X, Y], R) :-
+   to_ccodes(X, S1),
+   to_ccodes(Y, S2),
+   join(["[", "+", ",", " ", S1, ",", " ", S2, "]"], R), !.
+
+to_ccodes([==>, X, Y], R) :-
+   to_ccodes(X, S1),
+   to_ccodes(Y, S2),
+   join(["[", "==>", ",", " ", S1, ",", " ", S2, "]"], R), !.
+
+to_ccodes([*, X, Y], R) :-
+   to_ccodes(X, S1),
+   to_ccodes(Y, S2),
+   join(["[", "*", ",", " ", S1, ",", " ", S2, "]"], R), !.
+
+to_str(F, S) :-
+   to_ccodes(F, R),
+   atom_codes(S, R).
+
+
+
+%% Test and give message based on pass or failure.
+%% ======================================================================
+
+test(Predicate, Input, pass) :-
+
+   atom_codes(Predicate, P),
+   to_ccodes(Input, I),
+
+    ((call(Predicate, Input, _),
+      join(["passed: ", P, "(", I, ")"], CCodes),
+      atom_codes(S, CCodes), write(S))
+   ; (join(["failed: ", P, "(", I, ")"], CCodes),
+      atom_codes(S, CCodes), write(S))),
+
+   nl.
+
+test(Predicate, Input, fail) :-
    
-       
-:-
-   ((stree([==>, a, b], _),
-   write('Test 4 passed.')) ; write('Test 4 failed.')), nl.
+   atom_codes(Predicate, P),
+   to_ccodes(Input, I),
+
+    ((\+ call(Predicate, Input, _),
+      join(["passed: ", P, "(", I, ")"], CCodes),
+      atom_codes(S, CCodes), write(S))
+   ; (join(["failed: ", P, "(", I, ")"], CCodes),
+      atom_codes(S, CCodes), write(S))),
+
+   nl.
+
    
-% This one is a tautology.   
-:-
-   ((stree([==>, [*, a, [~, a]],
-                 [+, y, z]], _),
-   write('Test 5 passed.')) ; write('Test 5 failed.')), nl.
-   
+
+%% The tests.
+%% ======================================================================
+
+:- test(stree, [+, x, y], pass).
+
+:- test(stree, [+, x, [~, x]], pass).
+
+:- test(stree, [*, x, [~, x]], fail).
+
+:- test(stree, [==>, a, b], pass).
+
+% This one is a tautology.
+:- test(stree, [==>, [*, a, [~, a]],
+                     [+, y, z]], pass).
+
 % Its negation is unsatisfiable.
-:-
-   ((\+ stree([~, [==>, [*, a, [~, a]],
-                        [+, y, z]]], _),
-   write('Test 6 passed.')) ; write('Test 6 failed.')), nl.
+:- test(stree, [~, [==>, [*, a, [~, a]],
+                         [+, y, z]]], fail).
+
+% Another tautology.
+:- test(stree, [+, x,
+                   [==>, p,
+                         [+, p,
+                             [~, x]]]], pass).
+              
+% Its negation is unsatisfiable.
+:- test(stree, [~, [+, x, [==>, p, [+, p, [~, x]]]]], fail).
+
+% Satisfiable by p:false, x:false, y:true
+:- test(stree, [*, [*, [~, p], [+, x, y]],
+                   [+, [~, p], [==>, p, [+, p, [~, x]]]]], pass).
+
+:- test(stree, [*, [+, [~, p], [+, x, y]], p], pass).
+
+:- test(stree, [==>, p, [+, p, [~, x]]], pass).
+
+
 
